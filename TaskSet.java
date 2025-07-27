5ecd8f3df5eec269d257da32b6d267409ade0db7
Boris9777 <Strike34-Boris@yandex.ru>
2025-07-27 17:59:41 +0300

    Homework 2

---
+++ TaskSet.java
@@ -0,0 +1,112 @@
+public class TaskSet {
+
+    // 1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    // 2
+    public static void checkSumSign() {
+        int a = 10, b = -5;
+        int sum = a + b;
+        System.out.println(sum >= 0 ? "Сумма положительная" : "Сумма отрицательная");
+    }
+
+    // 3
+    public static void printColor() {
+        int value = 50;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    // 4
+    public static void compareNumbers() {
+        int a = 7, b = 9;
+        System.out.println(a >= b ? "a >= b" : "a < b");
+    }
+
+    // 5
+    public static boolean isSumBetween10And20(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    // 6
+    public static void printPositiveOrNegative(int number) {
+        System.out.println(number >= 0 ? "Число положительное" : "Число отрицательное");
+    }
+
+    // 7
+    public static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    // 8
+    public static void printStringNTimes(String text, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(text);
+        }
+    }
+
+    // 9
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
+    // 10
+    public static void invertArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] == 0 ? 1 : 0;
+        }
+    }
+
+    // 11
+    public static int[] fillArray100() {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        return arr;
+    }
+
+    // 12
+    public static void doubleIfLessThan6(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+    }
+
+    // 13
+    public static void fillDiagonal(int size) {
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = 1;
+        }
+
+        // Вывод матрицы
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // 14
+    public static int[] createArray(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+        }
+        return arr;
+    }
+}
