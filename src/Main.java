343cbfddaba09ea0e4203c19d3871486cea4dff1
Boris9777 <Strike34-Boris@yandex.ru>
2025-07-30 23:16:21 +0300

    Работа 3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Product[] productsArray = new Product[5];
 
         productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
-        productsArray[1] = new Product("iPhone 14 Pro", "15.01.2025", "Apple Inc.", "USA", 6299, false);
+        productsArray[1] = new Product("iPhone 13 Pro", "15.01.2025", "Apple Inc.", "USA", 6299, false);
         productsArray[2] = new Product("Xiaomi Mix 6", "20.12.2024", "Xiaomi Ltd.", "China", 4299, true);
         productsArray[3] = new Product("Sony Xperia Z5", "05.11.2024", "Sony Corp.", "Japan", 3999, false);
         productsArray[4] = new Product("Nokia Legend", "09.10.2024", "Nokia", "Finland", 2899, true);
