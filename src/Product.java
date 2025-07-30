d77ed6c46ee4c212641d030618b2232faa7c2670
Boris9777 <Strike34-Boris@yandex.ru>
2025-07-30 23:10:29 +0300

    Работа 3

---
+++ src/Product.java
@@ -0,0 +1,29 @@
+public class Product {
+    String name;
+    String productionDate;
+    String manufacturer;
+    String originCountry;
+    double price;
+    boolean isReserved;
+
+
+    public Product(String name, String productionDate, String manufacturer, String originCountry, double price, boolean isReserved) {
+        this.name = name;
+        this.productionDate = productionDate;
+        this.manufacturer = manufacturer;
+        this.originCountry = originCountry;
+        this.price = price;
+        this.isReserved = isReserved;
+    }
+
+
+    public void printInfo() {
+        System.out.println("Название: " + name);
+        System.out.println("Дата производства: " + productionDate);
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Страна: " + originCountry);
+        System.out.println("Цена: " + price);
+        System.out.println("Забронировано: " + (isReserved ? "Да" : "Нет"));
+        System.out.println("--------------------------------");
+    }
+}
