ccc9a65750a62db1074de68aae23b0b2abe43b09
Boris9777 <Strike34-Boris@yandex.ru>
2025-08-08 09:18:28 +0300

    Работа 5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(3, "Красный", "Чёрный");
-        Rectangle rect = new Rectangle(4, 5, "Синий", "Белый");
-        Triangle tri = new Triangle(3, 4, 5, "Зелёный", "Жёлтый");
 
-        circle.show();
-        rect.show();
-        tri.show();
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "X", "16"}
+        };
+
+        try {
+            int result = ArrayProcessor.processArray(array);
+            System.out.println("Сумма элементов массива: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных массива: " + e.getMessage());
+        }
+
+
+        try {
+            int[] numbers = {1, 2, 3};
+            System.out.println(numbers[5]); // Индекс вне диапазона
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Исключение: выход за пределы массива — " + e.getMessage());
+        }
     }
-}
+    }
