6c92536b10897c5b527e7e89d2d751acdff771cf
Boris977 <133016081+Boris977@users.noreply.github.com>
2025-08-24 22:01:01 +0300

    Update BlockTitleTest.java
    Изменения

--- src/main/java/org/example/BlockTitleTest.java
+++ src/main/java/org/example/BlockTitleTest.java
@@ -12,9 +12,14 @@ public class BlockTitleTest extends BaseTest {
     @Test
     public void checkBlockTitle() {
         driver.get("https://www.mts.by/");
-        WebElement title = driver.findElement(By.xpath("//h2[contains(text(),'Онлайн пополнение без комиссии')]"));
+
+        WebElement title = driver.findElement(By.xpath("//h2[contains(text(),'Онлайн пополнение')]"));
         assertTrue(title.isDisplayed(), "Заголовок блока не отображается");
-        assertEquals("Онлайн пополнение без комиссии", title.getText().trim());
+
+        
+        String actualText = title.getText().replace("\n", " ").trim();
+        String expectedText = "Онлайн поплнение без комиссии";
+
+        assertEquals(expectedText, actualText, "Текст заголовка не совпадает");
     }
 }
-
