25b770ebef6246bfacfc3785701e308e8e1a891e
Boris977 <133016081+Boris977@users.noreply.github.com>
2025-08-25 12:22:15 +0300

    Update BaseTest.java
    изменения

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -12,15 +12,15 @@ public class BaseTest {
 
     @BeforeEach
     public void setup() {
-        // ✅ Указываем путь к ChromeDriver версии 139
+        
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\strik\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
 
-        // ✅ Указываем путь к Chrome for Testing версии 139
+        
         ChromeOptions options = new ChromeOptions();
         options.setBinary("C:\\Users\\strik\\Downloads\\chrome-win64 (2)\\chrome-win64\\chrome.exe");
 
-        // 🔧 Инициализируем драйвер с нужным бинарником
+        
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
